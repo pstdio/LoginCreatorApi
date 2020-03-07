ea51b0db48056d74f31adcb5b6a2a7fbc7092c74
mbogudal <m.bogudal@linux.pl>
2020-03-07 22:21:14 +0100

    fix for LoginFactory

--- src/main/java/eu/pstdio/LoginCreatorApi/business/factory/LoginFactory.kt
+++ src/main/java/eu/pstdio/LoginCreatorApi/business/factory/LoginFactory.kt
@@ -1,7 +1,8 @@
 package eu.pstdio.LoginCreatorApi.business.factory
 
 import eu.pstdio.LoginCreatorApi.business.model.InputMessage
+import eu.pstdio.LoginCreatorApi.integration.entity.LoginEntity
 
 interface LoginFactory {
-	fun create(credentials : InputMessage)
+	fun create(credentials : InputMessage) : LoginEntity
 }
